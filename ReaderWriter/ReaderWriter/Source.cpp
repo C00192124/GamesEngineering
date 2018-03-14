7be31d4b98f3f9a908104bd99a7a972ddabdb000
Marc Gee <C00192124@itcarlow.ie>
2018-03-14 10:44:48 +0000

    reader/writer added, semaphore dumbed down

--- ReaderWriter/ReaderWriter/Source.cpp
+++ ReaderWriter/ReaderWriter/Source.cpp
@@ -1,8 +1,87 @@
 #pragma once
-#include "Semaphore.h"
+#include <iostream>
 #include <thread>
+#include <chrono>
+
+using namespace std;
+
+struct Semaphore
+{
+	int mutexReader = 1, mutexDB = 1;
+};
+
+void Reader();
+void Writer();
+
+void P(int &x);
+void V(int &x);
+
+Semaphore semaphore;
+
+int readerCount = 0;
 
 void main()
 {
+	thread tReader(Reader);
+	thread tWriter(Writer);
 
+	tReader.join();
+	tWriter.join();
+}
+
+//Try
+void P(int &x)
+{
+	while (x <= 0)
+	{
+		cout << "waiting..." << endl;
+	}
+	x -= 1;
+}
+
+//Increment
+void V(int &x)
+{
+	x += 1;
+}
+
+void Reader()
+{
+	bool running = true;
+	while (running)
+	{
+		cout << this_thread::get_id() << endl;
+		this_thread::sleep_for(chrono::seconds(1));
+		P(semaphore.mutexReader);
+		readerCount += 1;
+		if (readerCount == 1)
+		{
+			P(semaphore.mutexDB);
+		}
+		V(semaphore.mutexReader);
+		
+		cout << "Reading..." << endl;
+		
+		P(semaphore.mutexReader);
+		readerCount -= 1;
+		if (readerCount == 0)
+		{
+			V(semaphore.mutexDB);
+		}
+		V(semaphore.mutexReader);
+	}
+}
+void Writer()
+{
+	bool running = true;
+	while (running)
+	{
+		cout << this_thread::get_id() << endl;
+		this_thread::sleep_for(chrono::seconds(1));
+		P(semaphore.mutexDB);
+		
+		cout << "Writing..." << endl;
+
+		V(semaphore.mutexDB);
+	}
 }
